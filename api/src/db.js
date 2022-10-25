670c154f8c34ef59358625f940085ba2c48bef92
Francisco Porta <franciscoporta19@gmail.com>
2022-10-25 10:31:16 -0300

    Deploy 1.2

--- api/src/db.js
+++ api/src/db.js
@@ -11,11 +11,12 @@ const {
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 // });
 
-
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+
 
 const basename = path.basename(__filename);
 
